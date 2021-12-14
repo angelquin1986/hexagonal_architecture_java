59d94b4d399ebd00d92c109de44365f19bca7165
angel.quingaluisa@rappi.com <angel.quingaluisa@rappi.com>
2021-12-14 09:46:41 -0500

    inicitial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE country (
+                         id   INTEGER      NOT NULL AUTO_INCREMENT,
+                         rappiId   INTEGER      NOT NULL AUTO_INCREMENT,
+                         nameRt VARCHAR(128) NOT NULL,
+                         status INTEGER NOT NULL,
+                         PRIMARY KEY (id)
+);
